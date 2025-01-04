7bf40c5cf03e0590e1dea8f59b4d55c57bfeec00
L0o0k1 <ahmedomxo1@tutanota.com>
2025-01-04 19:06:07 +0200

    Some Edit

--- A-Num-Guesser/guesser.py
+++ A-Num-Guesser/guesser.py
@@ -12,10 +12,10 @@ else:
     quit()
 
 rNum = random.randint(0, what_range)
-guesses = 0 # keeping track of guesses made by player
+guesses = 0
 
 while True:
-    guesses += 1 #add +1 for every guess the user makes 
+    guesses += 1
     user_guess = input("Make a Guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
@@ -26,7 +26,7 @@ while True:
     if user_guess == rNum:
         print("You Got It!")
         print("You Got It In", guesses, "guesses!")
-        break # Exit loop if the guess is correct
+        break
     elif user_guess > rNum:
         print("You Are Above The Number!")
     else:
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## **This Repo Contain 9 Simple Python Projects**
 
-1- A Quiz Game - Easy.
-2- A Number Guesser Game - Easy.
-3- A Password Generator - Easy.
+1. A Quiz Game - Easy.
+2. A Number Guesser Game - Easy.
+3. A Password Generator - Easy.
